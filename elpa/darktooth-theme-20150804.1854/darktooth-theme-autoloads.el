04f3ef452f50e10cda3e00c0c9d15fb33a097492
raffled <raffled@gmail.com>
2015-08-09 15:11:09 -0400

    changed to darktooth theme

---
+++ elpa/darktooth-theme-20150804.1854/darktooth-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; darktooth-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "darktooth-theme" "darktooth-theme.el" (21954
+;;;;;;  21617 819775 757000))
+;;; Generated autoloads from darktooth-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("darktooth-theme-pkg.el") (21954 21617
+;;;;;;  859171 208000))
+
+;;;***
+
+(provide 'darktooth-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; darktooth-theme-autoloads.el ends here
---
+++ elpa/darktooth-theme-20150804.1854/darktooth-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "darktooth-theme" "20150804.1854" "Color theme for Emacs, when soothe and gruvbox collide" (quote nil))
